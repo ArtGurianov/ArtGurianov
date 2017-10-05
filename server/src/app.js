d074a8ecf64efb3c02cfdad44d48a2ad855c87ec
Арт Гурьянов <art@MacBook-Air-Art.local>
2017-10-05 18:08:55 +0900

    Auth completed. Session tokens. All verification done...

--- server/src/app.js
+++ server/src/app.js
@@ -4,9 +4,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const twoFactor = require('node-2fa');
+const {SHA256} = require('crypto-js');
 
-var newSecret = 'HK5LSXAMSDXU7BQ3GEEMACVKVCTLVRVV'//twoFactor.generateSecret({name: 'artgurianov', account: 'artgurianovcom'});
-var newToken = undefined;
+var authSecret = 'HK5LSXAMSDXU7BQ3GEEMACVKVCTLVRVV'//twoFactor.generateSecret({name: 'artgurianov', account: 'artgurianovcom'});
+var authToken = undefined; // valid for 1 minute to login
+var	sessionToken = undefined; //valid for 10 minutes to update content
 
 
 const app = express();
@@ -16,25 +18,39 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.get('/api/generate_token', (req, res) => {
-	newToken = twoFactor.generateToken(newSecret); //newSecret.secret
-	console.log(newToken);
-	setTimeout(function(){ newToken = undefined }, 60000);
+app.get('/api/generate_authToken', (req, res) => {
+	authToken = twoFactor.generateToken(authSecret); //authSecret.secret
+	console.log(authToken);
+	setTimeout(function(){ authToken = undefined }, 60000);
 	res.send({status: "TOKEN CREATED"});
 });
 
-app.post('/api/validate_token', (req, res) => {
+app.post('/api/validate_authToken', (req, res) => {
 
-	var result = twoFactor.verifyToken(newSecret, req.body.authcode); //newSecret.secret
+	var result = twoFactor.verifyToken(authSecret, req.body.authcode); //authSecret.secret
 
 	if (result === null) {
-		res.send('There was an error. Please try again later...');
+    res.statusCode = 302;
+    res.setHeader("Location", "http://localhost:8080/auth.html");
+    res.end();
+	} else if (result.delta < 0) {
+    res.statusCode = 302;
+    res.setHeader("Location", "http://localhost:8080/auth.html");
+    res.end();
+	} else if (result.delta >= 0) {
+		sessionToken = SHA256(authToken).toString();
+    setTimeout(function(){ sessionToken = undefined }, 600000);
+    res.statusCode = 302;
+    res.setHeader("Location", "http://localhost:8080/admin.html?sessionToken=" + sessionToken);
+    res.end();
 	}
-	if (result.delta < 0) {
-		res.send('This key has already expired. Please try again...');
-	}
-	if (result.delta >= 0) {
-		res.send('SUCCESS');
+});
+
+app.post('/api/validate_sessionToken', (req, res) => {
+	if (req.body.sessionToken === sessionToken) {
+		res.send({status: "OK"});
+	} else {
+		res.send({status: "FAILED"});
 	}
 });
 
